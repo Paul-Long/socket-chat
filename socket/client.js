3f6bdb6097418bf67db98a1755819675e0770524
候宝龙 <houbaolong@niwodai.net>
2018-03-08 17:30:05 +0800

    socket.io

--- socket/client.js
+++ socket/client.js
@@ -1,8 +1,12 @@
 const io = require('socket.io-client');
 
 function Socket(host) {
-
+  const socket = io(host || window.location.origin);
 }
+
+Socket.prototype.subscribe = function (channel) {
+
+};
 
 module.exports = function (host) {
   return new Socket(host);
