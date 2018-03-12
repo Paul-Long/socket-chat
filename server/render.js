c6bbede889a659ad953b31ae9c3ac09d1170c8b7
候宝龙 <houbaolong@niwodai.net>
2018-03-12 16:16:32 +0800

    use render template

--- server/render.js
+++ server/render.js
@@ -1,44 +1,13 @@
 const order = ['runtime', 'common', 'vendor', 'main'];
+const Render = require('render-template');
 
-const toArray = (chunk) => {
-  return Array.isArray(chunk) ? chunk : [chunk];
+module.exports = (req, chunks) => {
+  return new Render(chunks, order)
+    .meta({charset: 'utf-8'})
+    .meta({name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'})
+    .meta({name: 'theme-color', content: '#000000'})
+    .link({rel: 'shortcut icon', href: '/favicon.ico'})
+    .title('Socket.IO Chat')
+    .container()
+    .toString();
 };
-
-function getChunks(chunks, source) {
-  let js = [], css = [];
-  source.forEach(key => {
-    let chunk = chunks[key] || [];
-    chunk = toArray(chunk);
-    chunk.forEach(path => {
-      if (path.endsWith('.js')) {
-        js.push(path);
-      } else if (path.endsWith('.css')) {
-        css.push(path);
-      }
-    });
-  });
-  return {js, css};
-}
-
-const render = (chunks) => {
-  const {js = [], css = []} = getChunks(chunks, order);
-  return `
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#000000">
-  <link rel="shortcut icon" href="/favicon.ico">
-  <title>Socket.IO Chat</title>
-  ${css.map(path => `<link rel="stylesheet" href="/${path}" />`).join('\n') }
-  </head>
-  <body>
-    <div id="app"></div>
-    ${js.map(path => `<script src="/${path}"></script>`).join('\n')}
-  </body>
-</html>		
-  `
-};
-
-exports['default'] = (req, chunks) => render(chunks);
-module.exports = (req, chunks) => render(chunks);
